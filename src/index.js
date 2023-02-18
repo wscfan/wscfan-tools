239f0103e2f80c3ec7915751afe2ce8eb60c3a56
ws <wshappyday@sina.com>
2023-02-19 02:41:37 +0800

    v1.0.2

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
-const app = {
-  name: "wscfan",
-  say() {
-    console.log("hello, wscfan")
-  }
+function formatSigleNum(n) {
+  return n >= 10 ? "" + n : "0" + n;
 }
 
-module.exports = app
+export function formatNow() {
+  let now = new Date();
+  let year = now.getFullYear();
+  let month = now.getMonth() + 1;
+  month = formatSigleNum(month);
+  let day = now.getDate();
+  day = formatSigleNum(day);
+  let hour = now.getHours();
+  hour = formatSigleNum(hour);
+  let minute = now.getMinutes();
+  minute = formatSigleNum(minute);
+  let second = now.getSeconds();
+  second = formatSigleNum(second);
+  return `${year}-${month}-${day} ${hour}:${minute}:${second}`;
+}
+
+console.log(formatNow());
